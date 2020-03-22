666430cf8ea4bc6db13848c8c8d06bbf948e7a08
Marcus Menezes <mvgmenezes@gmail.com>
2020-03-22 12:44:51 +0200

    Trabalhando com maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'question.dart';
+import 'answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,7 +24,20 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
 
-    var questions = ['What\'s your favorite color?', 'What is your favorite animal?'];
+    var questions = [
+      {
+        'questionText': 'What\'s your favorite color?', 
+        'answers':['Black','Red','Green','White']
+      },
+      {
+        'questionText': 'What is your favorite animal?', 
+        'answers':['Rabbit','Snake','Elephant','Lion']
+      },
+      {
+        'questionText': 'Who is your favorite instructor?', 
+        'answers':['Marcus','Marcus','Marcus']
+      } 
+    ];
 
     return MaterialApp(
       home: Scaffold(
@@ -31,10 +45,13 @@ class _MyAppState extends State<MyApp> {
           title: Text("My fist text"),
         ),
         body: Column(children: [
-          Question(questions[_questionIndex]),
-          RaisedButton(child: Text('Answer 1'),onPressed: _answerQuestion,),
-          RaisedButton(child: Text('Answer 2'),onPressed: _answerQuestion,),
-          RaisedButton(child: Text('Answer 3'),onPressed: _answerQuestion,)
+          Question(questions[_questionIndex]['questionText']),
+          //recupera as respostas do json na posicao _questionIndex, e convert para uma List para poder realizar o map. 
+          //dentro de cada elemento do MAP retonra o new widget, convert para uma lista e apos isso é necessario usar o 
+          //spread ... para recupear os valores e dar um append dentro do children do Column.  
+          ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+            return Answer(_answerQuestion,answer);
+          }).toList()
         ],),
       ),
     );
